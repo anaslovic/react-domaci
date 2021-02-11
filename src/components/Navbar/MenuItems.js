1303701a38d739aba3bb805e9d6afa69fc00365c
anaslovic <slovicana0@gmail.com>
2021-02-11 06:09:34 +0100

    uradjen navbar

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    {
+        title: 'Pocetna',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Cenovnik',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Zakazivanje',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+]
